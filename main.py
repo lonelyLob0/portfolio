e540ac790536217078e816937ba52deb711157a2
lonelyLob0 <abab1000@mail.com>
2023-02-07 10:54:40 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+# -------------------- Flask variables --------------------
+app = Flask('__app__')
+app.config['SECRET_KEY'] = os.environ.get('PORTFOLIO_FLASK_KEY')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
