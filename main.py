2abfed4b8c0d585067b67c96dd9438316b68419b
lonelyLob0 <abab1000@mail.com>
2023-02-16 14:02:37 -0600

    home.html changed to index.html.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = os.environ.get('PORTFOLIO_FLASK_KEY')
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
