f8638370c1f5f66381db1a18c5b0e77dbb450a25
lonelyLob0 <abab1000@mail.com>
2023-04-04 10:54:15 -0600

    Run in production mode has been updated.

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def contact():
 if __name__ == '__main__':
     # with app.app_context():
     #     db.create_all()
-    app.run(debug=True)
-    # app.run()
+    #app.run(debug=True)
+    app.run()
 
 
